044cf184d5b52957665028aa204fdf8f6f4c3ebb
neha <nehasunilsahu1682gmail.com>
2021-01-12 00:02:57 +0530

    commited home page

--- electonic-voting-system-app/src/App.js
+++ electonic-voting-system-app/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-import AddElection from './components/add_election'
+import AddElection from './components/home'
+
 
 function App() {
   return (
     <div className="App">
      <AddElection></AddElection>
+    
     </div>
   );
 }
