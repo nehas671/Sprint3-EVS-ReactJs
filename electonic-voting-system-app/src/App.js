338928f71f978f30cdfcb89c5bc474fa8bd7bc49
neha <nehasunilsahu1682gmail.com>
2021-01-12 12:06:35 +0530

    commitied viewelection component

--- electonic-voting-system-app/src/App.js
+++ electonic-voting-system-app/src/App.js
@@ -1,15 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
 import Home from './components/home'
+import AddElection from './components/addElection'
+import ShowElections from './components/viewElection'
+import { BrowserRouter as Router,  Switch,  Route,  Link } from "react-router-dom";
 
 
 function App() {
   return (
     <div className="App">
-     <Home></Home>
-    
-    </div>
+    {/*<Home></Home>*/}
+   {/*<AddElection></AddElection>*/}
+    <ShowElections/>
+   </div>
   );
+
+
+ 
 }
 
 export default App;
