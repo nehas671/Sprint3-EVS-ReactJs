deac576bf132861674d44c722bbcf50a303ca6a4
neha <nehasunilsahu1682gmail.com>
2021-01-12 14:05:09 +0530

    commited

---
+++ electonic-voting-system-app/src/App.js
@@ -0,0 +1,31 @@
+import logo from './logo.svg';
+import './App.css';
+import Home from './components/home'
+import AddElection from './components/addElection'
+
+import ShowElections from './components/viewElection'
+import AddResult from './components/addresult';
+import { BrowserRouter as Router,  Switch,  Route,  Link } from "react-router-dom";
+
+
+function App() {
+  return (
+    <div className="App">
+
+    <Home></Home>
+  
+  { /*<AddElection></AddElection>*/}
+   { /*<ShowElections/>*/}
+
+    
+   { /*<AddElection></AddElection>*/}
+  
+
+   </div>
+  );
+
+
+ 
+}
+
+export default App;
