bd5cd1a16de6aac8e078964f825a473348a463de
priya shetpal <priya.shetpal07@gmail.com>
2021-01-12 10:59:32 +0530

    commit

---
+++ electonic-voting-system-app/src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+import Home from './components/home'
+import AddElection from './components/addElection'
+import AddResult from './components/addresult';
+import { BrowserRouter as Router,  Switch,  Route,  Link } from "react-router-dom";
+
+
+function App() {
+  return (
+    <div className="App">
+    <Home></Home>
+   { /*<AddElection></AddElection>*/}
+  
+   </div>
+  );
+
+
+ 
+}
+
+export default App;
