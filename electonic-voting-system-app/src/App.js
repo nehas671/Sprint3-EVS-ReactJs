94ea46c644ef080f0f42fcd515cea6b00fac5306
priya shetpal <priya.shetpal07@gmail.com>
2021-01-12 10:27:47 +0530

    commit

--- electonic-voting-system-app/src/App.js
+++ electonic-voting-system-app/src/App.js
@@ -1,12 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import AddElection from './components/home'
-
+import {AddResult} from './components/addresult';
 
 function App() {
   return (
     <div className="App">
-     <AddElection></AddElection>
+     {/*<AddElection></AddElection>*/}
+<AddResult></AddResult>
     
     </div>
   );
