a6a18c1b21b6f2aa8afa69e138d4eee5721f20d3
jyoti <jyotitiwari631@gmail.com>
2021-01-12 08:24:59 +0530

    Commiting

--- electonic-voting-system-app/src/App.js
+++ electonic-voting-system-app/src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
-import AddElection from './components/add_election'
+import AddElection from './components/add_election';
+import ApproveRequest from './components/approve_request';
+import AddVoterRequest from './components/add_voter_request';
 
 function App() {
   return (
     <div className="App">
-     <AddElection></AddElection>
+     
+
     </div>
   );
 }
---
+++ electonic-voting-system-app/src/components/approve_request.js
@@ -0,0 +1,21 @@
+const ApproveRequest = (props)=>{
+
+    return (
+        <div>
+            <br></br>
+        <h2>Approve/Reject Voter Request</h2>
+        <br></br>
+        <br></br>
+        
+            <form action="">
+                <label for='Name'>Name</label>
+                <input type="text" name="Name" id="name" class="form-control"></input>
+                <label for="Contact Number">Contact Number</label>
+                <input type="text" name="Contact Number"></input>
+
+            </form>
+        </div>
+    )
+}
+
+export default ApproveRequest
