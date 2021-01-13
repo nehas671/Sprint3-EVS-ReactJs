85b5590a2835aefc5b2b4d7621a49072016e1eeb
jyoti <jyotitiwari631@gmail.com>
2021-01-13 16:23:59 +0530

    committing

--- electonic-voting-system-app/src/components/addVoterRequest.js
+++ electonic-voting-system-app/src/components/addVoterRequest.js
@@ -4,6 +4,9 @@ import * as icons from '@fortawesome/free-solid-svg-icons'
 import { useSelector, useDispatch } from 'react-redux';
 import { Button } from 'react-bootstrap';
 import VoterRequest from '../models/VoterRequest';
+import Slogan from "./slogan";
+import Aside from "./aside";
+import Footer from './footer';
 
 
 let dispatch;
@@ -13,17 +16,76 @@ const AddRequest = (props) => {
    
     dispatch = useDispatch();
    
+  /*  React.useEffect(() => {
+        DistrictsList()
+      }, []);
+    
+      const DistrictsList = () => {
+        dispatch(showDistrictsAction())
+      }
+
+    console.log("StatesList: ", statlist);*/
 
     const addVoterRequest = ()=>{
         
     }
     return (
     <div>
-    <div>
     
+    <header class="Custom-container py-md-2 py-3   ">
+  <div class="header-before"><span class="mr-4"><img src="logo.jpg" alt="brand-name" class="logo"/></span><h1 class="d-inline">Electronic Voting System</h1></div>
+        <nav class="navbar navbar-expand-md  navbar-light d-md-block d-lg-flex px-sm-0 py-0 text-wrap ">
+
+
+            {/*<div class="navbar-brand nav-custom-brand mb-3 mb-md-0 py-0">
+                
+    </div>*/}
+            <button class="navbar-toggler  custom-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav flex-wrap w-100">
+                    <li class="nav-item  nav-item-custom mb-2 mb-md-0 flex-wrap ">
+                        <a class="nav-link text-dark border-primary nav-custom-link px-md-0 " href="#">Home</a>
+                    </li>
+                    <li class="nav-item  nav-item-custom  mb-2 mb-md-0 flex-wrap ">
+                        <a class="nav-link text-dark px-md-0 border-primary nav-custom-link  " href="#">About Us</a>
+                    </li>
+                    <li class="nav-item nav-item-custom  mb-2 mb-md-0 flex-wrap">
+                        <a class="nav-link text-dark px-md-0 border-primary  nav-custom-link" href="#">Contact Us</a>
+                    </li>
+                    <li class="nav-item nav-item-custom  mb-2 mb-md-0 flex-wrap">
+                        <a class="nav-link text-dark px-md-0  border-primary  nav-custom-link " href="#">Voter Request</a>
+                    </li>
+                    <li class="nav-item nav-item-custom  mb-2 mb-md-0 flex-wrap">
+                        <a class="nav-link text-dark px-md-0  border-primary nav-custom-link " href="#">View Voter Request</a>
+                    </li>
+                </ul>
+
+                <div class=" d-flex sky-color search-box ">
+                    <input type="search" class=" border-0 sky-color  ml-md-auto" placeholder="search..."></input>
+                    <div class="">
+                        <button class=" btn search-button border-0 sky-color " type="button" id="search-button" ><FontAwesomeIcon icon={icons.faSearch} /></button>
+                    </div>
+                </div>
+
+
+            </div>
+        </nav>
+
+
+    </header>
+
+
     <main>
-        <div class="jumbotron sky-color jumbotron-custom mb-0 px-sm-0 rounded-0">
-        <div class="col-6 border border-dark p-5 ml-auto mr-auto">
+        <Slogan/>
+
+        <section class="Custom-container technology-container">
+            <div class="row mx-0 px-sm-0 mb-4">
+
+        <div class="col-8  pl-0 pr-5">
+        <div class="col border border-dark bg-light p-5 ml-auto mr-auto">
         <h2>Add Voter Request</h2>
         <br></br>
         <form>
@@ -94,9 +156,12 @@ const AddRequest = (props) => {
         </div>
         
 </div>
+        <Aside/>
+        </div>
+        </section>
 </main>
             
-</div>
+    <Footer/>
 
 
 </div>
