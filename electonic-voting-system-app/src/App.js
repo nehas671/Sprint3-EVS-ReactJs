11616994086412180f71d4ac9fae13e1bc023ad0
Rishabh <rishabh.chaurasiya1@gmail.com>
2021-01-14 23:38:19 +0530

    Added admin aside and made slight modifications in cast vote and voter services

--- electonic-voting-system-app/src/App.js
+++ electonic-voting-system-app/src/App.js
@@ -23,6 +23,7 @@ import AddCandidate from './components/addCandidate';
 import ViewParty from './components/view_party';
 import ViewVoterReq from './components/view_voterReq';
 import ApproveRequest from './components/approveVoterRequest'
+import CastVote from './components/castVote';
 //import ButtonForm from './components/button';
 
 
@@ -150,8 +151,9 @@ function App() {
           <Route exact path="/Officer_services">
             <OfficerService></OfficerService>
           </Route>
-
-          
+          <Route exact path='/votecast'>
+            <CastVote />
+          </Route>
      </Switch> 
 
          
