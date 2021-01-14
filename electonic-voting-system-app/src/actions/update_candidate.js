4ec102e6601eed83326177df57b8e7849439e113
Aachal <aachaldaharwal@gmail.com>
2021-01-14 20:41:13 +0530

    commiting

---
+++ electonic-voting-system-app/src/actions/update_candidate.js
@@ -0,0 +1,20 @@
+const UpdateCandidateAction = (candidateObj) => {
+    return async function(dispatch) {
+        const res = await axios.put(
+            "http://localhost:8080/evs/updatecandidate" + candidateObj.id,
+                { 
+                    candidateName: candidateObj.candidateName, 
+                    address: candidateObj.address,
+                    age: candidateObj.age,
+                    contact_number: candidateObj.contact_number, 
+                    email: candidateObj.email
+                }, 
+                { 
+                    "Content-type": "application/json; charset=UTF-8"
+                }
+            );
+          //console.log('Update Candidate serverResponse: ', res.data);
+          dispatch({type: "UPDATE_CANDIDATE", payload: res.data});
+
+    }
+}
