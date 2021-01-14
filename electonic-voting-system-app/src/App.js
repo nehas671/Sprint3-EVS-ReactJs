738228e4ee4141e71fc3f79e0623a876a21bd64b
gayatri <yeolegayatri61@gmail.com>
2021-01-14 19:52:02 +0530

    msg

--- electonic-voting-system-app/src/App.js
+++ electonic-voting-system-app/src/App.js
@@ -2,97 +2,61 @@ import './App.css';
 import Home from './components/home'
 
 import Officer from './components/electrorall_officer';
+import AddCandidate from './components/candidateForm';
 import ViewCandidates from './components/ViewCandidate';
 
 import AddElection from './components/addElection'
-import ViewResult from './components/viewresult'
+
 import ShowElections from './components/viewElection'
-import {AddResult} from './components/addresult';
+import AddResult from './components/addresult';
 import { BrowserRouter as Router,  Switch,  Route,  Link } from "react-router-dom";
 import CandidateForm from './components/candidateForm';
 import AddRequest from './components/addVoterRequest';
-import RegisterUser from './components/register_user';
+
+
 
 function App() {
   return(
     <div>
-
-
-{/* <AddElection></AddElection>*/}
-<<<<<<< HEAD
-   { /*<ShowElections/>*/}
-
-    
-   { /*<AddElection></AddElection>
-   <ViewCandidates></ViewCandidates>*/}
-
-   
-   {/*<CandidateForm></CandidateForm>*/}
-
-=======
-   
->>>>>>> 198785c73a1ab407b26ff729f892647ac3294440
-
-    
-   { /*<AddElection></AddElection>
-   <ViewCandidates></ViewCandidates>*/}
-
-   
-   {/*<CandidateForm></CandidateForm>
     
 
-
-
-
-   <RegisterUser></RegisterUser>
-
-   <Home></Home>*/}
- 
-{/*<AddResult></AddResult>*/}
-<ViewResult></ViewResult>
-   {/*<Router>
-
-
-
    
-
     <div className="App">
       
-<<<<<<< HEAD
-            <Link to="/"></Link>
-=======
            {/* <Link to="/">Home</Link>&nbsp;&nbsp;
             <Link to ="/add_candidate">Add </Link>&nbsp;&nbsp;
   <Link to ="/view_candidate">Show</Link>*/}
->>>>>>> d4883cb74461d59971c797eca3aa796a184a36ef
 
       <Switch>
           <Route exact path="/">
             <Home/>
           </Route>
+
+          <Route path="/add_candidate">
+            <AddCandidate/>
+          </Route>
+          <Route path="/view_candidate">
+          <ViewCandidates/>
+          </Route>
+
           <Route path="/election">
           <AddElection/>
           </Route>
           <Route path="/showelection">
           <ShowElections/>
           </Route>
-          <Route exact path="/RegisterUser">
-            <RegisterUser></RegisterUser>
+          <Route exact path="/addVoterRequest">
+            <AddRequest></AddRequest>
           </Route>
         </Switch> 
         </div>       
-           
-
-  { /*<AddElection></AddElection>*/}
-   { /*<ShowElections/>*/}
-
-
-    
-   { /*<AddElection></AddElection>*/}
-  
-
-   </div>);
- 
+       
+        
+    </div>
+  );
 }
+
+
+
 
 export default App;
