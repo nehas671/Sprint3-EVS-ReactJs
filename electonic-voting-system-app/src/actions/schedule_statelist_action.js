77ddf3fdcfe1fb7a9da0102cb76a601dcdf3d79f
Vaishnavi <vaishnavichippa23@gmail.com>
2021-01-15 19:05:45 +0530

    commit

--- electonic-voting-system-app/src/actions/schedule_statelist_action.js
+++ electonic-voting-system-app/src/actions/schedule_statelist_action.js
@@ -1,13 +1,9 @@
 import axios from 'axios';
 let viewScheduleStateListAction = () => {
+    console.log("state list:");
     return async function (dispatch) {
         const res = await fetch(
-            `http://localhost:8080/evs/schedule/state`, {
-                method: "GET",
-                headers: {
-                    "Access-Control-Allow-Origin": "*"
-                }
-            }
+            "http://localhost:9090/evs/schedule/statelist"
           );
           const data = await res.json();
           dispatch({type: "VIEW_SCHEDULE_STATE_LIST", payload: data});
